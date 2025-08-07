277bb7fc341dfdee2db2195d45d061fc22effc92
YDancer <149452536+YDancer@users.noreply.github.com>
2025-08-07 12:07:27 -0600

    Slideshow is functional
    needs additional styling, and an image indicator on the sides to show that it is in fact a slideshow

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -3,9 +3,13 @@ import Sidebar from "./Sidebar";
 // Project is the conglomerate of the image slideshow
 // Necessary sidebars and necessary text and headers
 export default function Project(){
+    const imageList = import.meta.glob("../assets/testImages1/*", {eager:true, import:"default"}); 
+    const imgs = Object.entries(imageList).map((i)=><img src={i[1]}></img>) // lowkey idk how this works
+    // if it works it works
+
     return(
         <section>
-            <Slideshow></Slideshow>
+            <Slideshow imageElements={imgs}></Slideshow>
         </section>
     )
 }
--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -1,9 +1,39 @@
 import sampleImage from "../assets/sampleImage.png"
 import "./Slideshow.css"
+import React from "react"
 
-export default function Slideshow(){
+// provide list of image elements for slideshow from upper component
+
+
+export default function Slideshow(props){
+    const images = props.imageElements
+    const [selectedImage, setSelectedImage] = React.useState(0);
+
+    function leftImage(){
+        setSelectedImage((prevValue)=>{
+            if (prevValue == 0)
+                return(Object.keys(images).length-1)
+            else
+                return(prevValue-1)
+        })
+    }
+
+    function rightImage(){
+        setSelectedImage((prevValue)=>{
+            if (prevValue == Object.keys(images).length-1)
+                return(0)
+            else
+                return(prevValue+1)
+        })
+    }
 
     return(
-        <img src={sampleImage}></img>
+        <div className="slideshowContainer">
+            <button className="leftButton" onClick={leftImage}>Left</button>
+            <div className="slideshowImageContainer">
+                {images[selectedImage]}
+            </div>
+            <button className="rightButton" onClick={rightImage}>Right</button>
+        </div>
     )
 }
