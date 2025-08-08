9f7b44585f7879622fb8fd9f8cbfb099d96b74b6
YDancer <149452536+YDancer@users.noreply.github.com>
2025-08-07 19:27:11 -0600

    Cyclic slideshow with functioning neighbors
    Neighbouring images are now shown, and cycle correctly. Additional styling needed

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -7,33 +7,67 @@ import React from "react"
 
 export default function Slideshow(props){
     const images = props.imageElements
-    const [selectedImage, setSelectedImage] = React.useState(0);
 
-    function leftImage(){
-        setSelectedImage((prevValue)=>{
-            if (prevValue == 0)
-                return(Object.keys(images).length-1)
+    const [selectedImage, setSelectedImage] = React.useState(0);
+    const [leftImageIndex, setLeftImageIndex] = React.useState(recomputeImage(false, 0));
+    const [rightImageIndex, setRightImageIndex] = React.useState(recomputeImage(true, 0));
+
+    function recomputeImage(goingRight, currentIndex){ // Selects the correct index for the image specified
+        // Created so neighbouring images and current image can use the same function
+        if (goingRight){ // if true advance the slide forward (to the right)
+            if (currentIndex == Object.keys(images).length-1) // if max then loop all the way to the left
+                return 0
             else
-                return(prevValue-1)
+                return (currentIndex+1) // otherwise go right
+        } else {   // if false advance the slide backward (to the left)
+            if (currentIndex == 0)
+                return (Object.keys(images).length-1) // if min then loop all to the right
+            else
+                return (currentIndex-1) // Otherwise go left
+        }
+    }
+
+    function leftImage(){ // When the left arrow is clicked
+        setSelectedImage((prevValue)=>{
+            return(recomputeImage(false, prevValue))
+        })
+
+        setLeftImageIndex((prevValue)=>{
+            return(recomputeImage(false, prevValue))
+        })
+        
+        setRightImageIndex((prevValue)=>{
+            return(recomputeImage(false, prevValue))
         })
     }
 
-    function rightImage(){
-        setSelectedImage((prevValue)=>{
-            if (prevValue == Object.keys(images).length-1)
-                return(0)
-            else
-                return(prevValue+1)
+    function rightImage(){ // When the right arrow is clicked
+         setSelectedImage((prevValue)=>{
+            return(recomputeImage(true, prevValue))
+        })
+
+        setLeftImageIndex((prevValue)=>{
+            return(recomputeImage(true, prevValue))
+        })
+        
+        setRightImageIndex((prevValue)=>{
+            return(recomputeImage(true, prevValue))
         })
     }
 
     return(
         <div className="slideshowContainer">
+            <div className="slideshowLeftImage">
+                {images[leftImageIndex]}
+            </div>
             <button className="leftButton" onClick={leftImage}>Left</button>
             <div className="slideshowImageContainer">
                 {images[selectedImage]}
             </div>
             <button className="rightButton" onClick={rightImage}>Right</button>
+            <div className="slideshowRightImage">
+                {images[rightImageIndex]}
+            </div>
         </div>
     )
 }
