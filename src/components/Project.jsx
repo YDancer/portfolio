7a6e5aeda435118d743ad2d427add54293372dd9
YDancer <149452536+YDancer@users.noreply.github.com>
2025-08-16 02:37:44 -0600

    Project tab styling + implementation
    Added the layout styling to the project tab. Also added rough colors. These are currently not specific and affect the entire website, so make them independent.

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,5 +1,6 @@
 import Slideshow from "./Slideshow";
 import Sidebar from "./Sidebar";
+import "./Project.css"
 // Project is the conglomerate of the image slideshow
 // Necessary sidebars and necessary text and headers
 export default function Project(){
@@ -8,8 +9,25 @@ export default function Project(){
     // if it works it works
 
     return(
-        <section>
-            <Slideshow imageElements={imgs}></Slideshow>
+        <section className="projectContainer">
+            <div className="projectLeft">
+                <h2>Project Name</h2>
+                <Slideshow imageElements={imgs}></Slideshow>
+                <h3>Image Name</h3>
+                <p>Image description goes here and tends to be a little longer</p>
+            </div>
+            <div className="projectCenter">
+                <h2>Objective</h2>
+                <p>Text for objective tends to be a whole paragraph of text. A lot longer then the description
+                    Text for objective tends to be a whole paragraph of text. A lot longer then the description
+                    Text for objective tends to be a whole paragraph of text. A lot longer then the description
+                    Text for objective tends to be a whole paragraph of text. A lot longer then the description
+                </p>
+            </div>
+            <div className="projectRight">
+                <button>More on my role</button>
+                <button>External Links</button>
+            </div>
         </section>
     )
 }
