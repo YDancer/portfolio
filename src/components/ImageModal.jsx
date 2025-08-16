364b7029ce718ff215f82f78783a5e364076a5fe
YDancer <149452536+YDancer@users.noreply.github.com>
2025-08-16 01:59:41 -0600

    Rough Image Modal
    Needs more work

--- src/components/ImageModal.jsx
+++ src/components/ImageModal.jsx
@@ -1,11 +1,21 @@
 import "./ImageModal.css"
+import React from "react"
 
 export default function ImageModal(props){
     
-    
     return(
-    <div className="ImageModalOverlay" onClick={props.overlayHandler}>
-        {props.imageElements[props.currentIndex]}
-    </div>
+    <>
+        <div className="ImageModalOverlay">
+            <div className="ImageModalHeader">
+                <h2>Cat Image</h2>
+            </div>
+            {props.imageElements[props.currentIndex]}
+            <div className="ImageModalFooter">
+                {props.imageElements}
+            </div>
+        </div>
+        <div className="ImageModalBackground" onClick={props.overlayHandler}>
+        </div>
+    </>
 )
 } 
