60de516796329ee6bc27b5716d48bc1e9e3868b7
YDancer <149452536+YDancer@users.noreply.github.com>
2025-08-18 23:32:08 -0600

    Prepping for json
    This will need a little more thinking. Have to take out image paths, and import manually in a seperate js file.

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,9 +1,22 @@
+import Project from "./components/Project"
+import {default as projectList} from "./data/projects.json"
+
 export default function Body(){
     // Main content
     // This includes all the various section elements
     // Such as the projects tab, personal endeavors and additional media
     // 
+    let projElements = [];
+
+    function createProjects(projObj, index1){
+    }
+
+    projectList.forEach(createProjects)
+
+    
     return(
-        <></>
+        <>
+            <Project/>
+        </>
     )
 }
