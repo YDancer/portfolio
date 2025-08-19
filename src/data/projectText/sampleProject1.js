3e3585826b262febb14386fda37843a6c44fcacd
YDancer <149452536+YDancer@users.noreply.github.com>
2025-08-18 22:34:02 -0600

    sample json setup

---
+++ src/data/projectText/sampleProject1.js
@@ -0,0 +1,18 @@
+const projObj = ```
+This is a whole load of text and i figured this would be an easier way to store
+when i'm finished typing everything, i'll export this variable. 
+Now this is the objective of whatever i was making. So in essence what were we/I trying to acheive
+Or in essence what is the project about
+
+Generally there should be even more text for this objective, enough but this is just an example after all
+```;
+
+const moreRole = ```
+Hey guess what, this is another large area of text, dedicated to a sidebar. 
+This section is dedicated to things that I did specifically among the project
+now i'm keeping this one shorter for 
+
+the placeholder text anyways. I think it'll be fine
+```;
+
+export {projObj, moreRole};
---
+++ src/data/readme.txt
@@ -0,0 +1,10 @@
+
+Projects.json:
+    Projects json file assumes that the imageDir are in the src/assets/Projects folder
+    It also assumes that all files in the subfolder of ex: src/assets/Projects/proj1 are images
+
+    Furthermore, those images are assigned descriptions alphabetically
+    Ex: 1.png, a.jpg, 35.jpg should be ordered as follows: [1.png, 35.jpg, a.jpg]
+
+    Note that larger text sections (like objective, my role) will be stored in seperate js files, just for cleaner organization
+    Assuming that it is in the src/data/projectText folder
