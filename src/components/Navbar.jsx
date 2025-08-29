eae339d17af1c93a7e5fcc85b176ce21970f3aa3
YDancer <149452536+YDancer@users.noreply.github.com>
2025-08-29 13:02:30 -0600

    styling + icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,9 @@
 import "../components/Navbar.css"
 import logo1 from "../assets/kslogo.svg"
+import homeIcon from "../assets/home.svg"
+import projectIcon from "../assets/project.svg"
+import projectIcon2 from "../assets/otherProject.svg"
+import contactIcon from "../assets/contact.svg"
 
 // the navbar (used to navigate between different pages)
 // We might want to opt to have a seperate quick links from the home page too
@@ -18,13 +22,13 @@ export default function Navbar(){
             </a>
             
             <div className="navbarLeftContainer navbarContainer">
-                <a href="">Home</a>
-                <a href="Projects">Projects</a>
-                <a href="External Media">External Media</a>
+                <a href=""><img src={homeIcon}></img>Home</a>
+                <a href="Projects"><img src={projectIcon}></img>Projects</a>
+                <a href="External Media"><img src={projectIcon2}></img>External Media</a>
                 
             </div>
             <div className="navbarRightContainer navbarContainer">
-                <a href="Contact Me">Contact Me</a>
+                <a href="Contact Me"><img src={contactIcon}></img>Contact Me</a>
             </div>
         </nav>
     )
