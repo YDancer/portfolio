a6799f6a72f6c9aa7903b9f0a504384642aafca9
YDancer <149452536+YDancer@users.noreply.github.com>
2025-08-29 13:34:40 -0600

    comment

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,12 +8,10 @@ import contactIcon from "../assets/contact.svg"
 // the navbar (used to navigate between different pages)
 // We might want to opt to have a seperate quick links from the home page too
 // ex: Sidebar that jumps from section to section on the home page specifically
-
-
+// Currently the links only refer to pages only on the homepage
+// As I don't have enough experience for individual pages
 
 export default function Navbar(){
-    
-
     return(
         <nav className="navbar">
             <div className="navbarLeftContainer">
