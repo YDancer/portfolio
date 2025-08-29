6becf3461665ba7b3a72a55a1f775b6d2f8eb0ce
YDancer <149452536+YDancer@users.noreply.github.com>
2025-08-29 14:56:30 -0600

    colors + margins
    Rearrangements of some sections of code, and added margins

--- src/Body.jsx
+++ src/Body.jsx
@@ -1,4 +1,5 @@
 import Project from "./components/Project"
+import Overview from "./components/Overview"
 //import {default as projectList} from "./data/projects.json"
 
 import {default as projectList} from "./data/projImport";
@@ -23,8 +24,9 @@ export default function Body(){
     )});
     
     return(
-        <>
+        <main>
+            <Overview/>
             {projElements}
-        </>
+        </main>
     )
 }
