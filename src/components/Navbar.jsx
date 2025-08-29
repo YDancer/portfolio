3c42a14aa79f31189e23fc254a698f3ad91b7634
YDancer <149452536+YDancer@users.noreply.github.com>
2025-08-29 02:23:18 -0600

    Rough styling complete
    Spacing change if necessary, and add icons to each button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,31 @@
 import "../components/Navbar.css"
+import logo1 from "../assets/kslogo.svg"
 
 // the navbar (used to navigate between different pages)
 // We might want to opt to have a seperate quick links from the home page too
 // ex: Sidebar that jumps from section to section on the home page specifically
+
+
 
 export default function Navbar(){
     
 
     return(
         <nav className="navbar">
-            <ul className="navbarList">
-                <li><a href="">Home</a></li> {/** Replace with an image probably */}
-                <li><a href="Projects">Projects</a></li>
-                <li><a href="External Media">External Media</a></li>
-                <li><a href="Contact Me">Contact Me</a></li>
-            </ul>
+            
+            <a href="" className="navbarImg">
+                <img src={logo1}></img>
+            </a>
+            
+            <div className="navbarLeftContainer navbarContainer">
+                <a href="">Home</a>
+                <a href="Projects">Projects</a>
+                <a href="External Media">External Media</a>
+                
+            </div>
+            <div className="navbarRightContainer navbarContainer">
+                <a href="Contact Me">Contact Me</a>
+            </div>
         </nav>
     )
 }
