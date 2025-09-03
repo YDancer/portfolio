e179213b158fb9ed27a899b16f988a3d017e763d
YDancer <149452536+YDancer@users.noreply.github.com>
2025-09-03 00:06:44 -0600

    Contact layout created (styling needed)

---
+++ src/components/Contact.jsx
@@ -0,0 +1,14 @@
+import "./Contact.css"
+
+export default function Contact(){
+    return(
+        <section className="contactContainer">
+            <h2>Contact Me</h2>
+            <h3>Feel free to contact me via email or send me a message on LinkedIn</h3>
+            <div className="contactLinks">
+                <span>Email: <a>email@example.com</a> </span>
+                <span>LinkedIn: <a>link.example.com</a> </span>
+            </div>
+        </section>
+    )
+}
